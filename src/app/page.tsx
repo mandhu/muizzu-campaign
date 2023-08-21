0e77ba781c6ec72023fa43858fa70c9b5d81ace5
Mamdhooh Mohamed <mandhu305@gmail.com>
2023-08-21 20:29:37 +0500

    image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-        {/*<img src="/vote_4_mizzu.jpg" alt="Vote 4 muizzu" className="h-full"/>*/}
-        <Image
-            fill
-            quality={100}
-            src="/vote_4_mizzu.jpg"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            alt="Picture of the author"
-            objectFit={'contain'}
-        />
-    </main>
+      <Image
+          fill
+          quality={100}
+          src="/vote_4_mizzu.jpg"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          alt="Picture of the author"
+          objectFit={'contain'}
+      />
+    // <main className="flex min-h-screen items-center justify-center">
+    //     {/*<img src="/vote_4_mizzu.jpg" alt="Vote 4 muizzu" className="h-full"/>*/}
+    //
+    // </main>
   )
 }
